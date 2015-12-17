64180dbffd3bd4d916ed85b2b2368614b8ad8c24
andreaphillips <andrea@agilityfeat.com>
2015-12-17 17:21:59 -0600

    test

---
+++ Example/Pods/Target Support Files/Pods-Spotlight_Tests/Pods-Spotlight_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Spotlight_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Spotlight_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/Spotlight/Spotlight-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SpotlightVersionNumber;
+FOUNDATION_EXPORT const unsigned char SpotlightVersionString[];
+
